4eac38e4f365f692b0b1e3330096277b6e81e71f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-04-04 08:00:16 +0200

    reformat

--- Data/script.py
+++ Data/script.py
@@ -10,12 +10,18 @@ else:
     compteur = 0
     for i in liste_mots:
         if len(i) >= nombre_min_lettres:
-            compteur+=1
-    proportion = round(100*compteur/nombre_mots,2)
-    informations = {"Total":compteur,"Proportion":proportion,
-                   "Phrase":phrase,"Seuil":nombre_min_lettres}
-    sortie = f"Proportion de mots avec {nombre_min_lettres} lettres ou plus : {proportion}"
+            compteur += 1
+    proportion = round(100 * compteur / nombre_mots, 2)
+    informations = {
+        "Total": compteur,
+        "Proportion": proportion,
+        "Phrase": phrase,
+        "Seuil": nombre_min_lettres,
+    }
+    sortie = (
+        f"Proportion de mots avec {nombre_min_lettres} lettres ou plus : {proportion}"
+    )
     print(sortie)
     if sortie_fichier:
-        with open("resultat.txt","w") as f:
+        with open("resultat.txt", "w") as f:
             f.write(str(informations))
