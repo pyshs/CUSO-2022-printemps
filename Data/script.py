de82dc81260de0338ef567ebe0ace34dbcd8e930
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-04-04 08:04:54 +0200

    Script refactor:
    1) run black for standard formatting.
    
    2) don't use i as a loop variable name
    
    3) Introduce :0.1f formatter specifier, and round to 3 decimal.
       I don't Think we need to _talk_ about it, but it's interesting
       to have it just to say that there is more then just "{variable}" in
       strings formatting
    
    I really dont' like f.write(str(informations)), your should use
    json.dumps, but I know you don't want libraries

--- Data/script.py
+++ Data/script.py
@@ -1,4 +1,3 @@
-nombre_mots = 0
 sortie_fichier = True
 phrase = input("Écrivez une phrase: ")
 liste_mots = phrase.split(" ")
@@ -8,19 +7,17 @@ if nombre_mots == 0:
     print("La liste est vide")
 else:
     compteur = 0
-    for i in liste_mots:
-        if len(i) >= nombre_min_lettres:
+    for mot in liste_mots:
+        if len(mot) >= nombre_min_lettres:
             compteur += 1
-    proportion = round(100 * compteur / nombre_mots, 2)
+    proportion = round(100 * compteur / nombre_mots, 3)
     informations = {
         "Total": compteur,
         "Proportion": proportion,
         "Phrase": phrase,
         "Seuil": nombre_min_lettres,
     }
-    sortie = (
-        f"Proportion de mots avec {nombre_min_lettres} lettres ou plus : {proportion}"
-    )
+    sortie = f"Proportion de mots avec {nombre_min_lettres} lettres ou plus : {proportion:0.1f}"
     print(sortie)
     if sortie_fichier:
         with open("resultat.txt", "w") as f:
